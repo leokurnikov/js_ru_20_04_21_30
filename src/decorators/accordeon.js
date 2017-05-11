e78f202a20839ff6ddaacc4122495c40afafe877
Roman <kaboom.ua@gmail.com>
2017-05-11 11:25:26 +0300

    Update accordeon.js

--- src/decorators/accordeon.js
+++ src/decorators/accordeon.js
@@ -1,9 +1,11 @@
 import React, {Component as ReactComponent} from 'react'
 
+//DecoratedComponent я называл чтоб легче понять было. Лучше выбирай более значущее название
 export default (OriginalComponent) => class DecoratedComponent extends ReactComponent {
   constructor(props) {
     super()
     this.state = {
+        //Не привязывайся к названиям сущностей, вся суть декораторов в универсальности. Сделай openItemId
       openArticleId: null
     }
   }
